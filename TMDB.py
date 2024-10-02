f8af5746ecf55dc6be9e8b5a85c2b04ff3b99919
IYIatthys <31695309+IYIatthys@users.noreply.github.com>
2024-10-02 15:46:41 +0200

    Update TMDB.py
    Overhauled the script so:
    
    Not all images are removed prior to executing the script; 
    Added the possibility to loop through list, changing the amount of images outputted; 
    Edited the displayed date format to MM-YYYY; 
    Added a bool that dictates whether the images should be shared directly with the Android TV; 
    General cleanup

--- TMDB.py
+++ TMDB.py
@@ -5,69 +5,69 @@ import os
 import shutil
 from urllib.request import urlopen
 import textwrap
+from typing import List, Dict, Tuple
+import ShieldShare
 
-# Base URL for the API
-url = "https://api.themoviedb.org/3/"
-
-# Set your TMDB API Read Access Token key here
-headers = {
+# Constants
+API_URL = "https://api.themoviedb.org/3/"
+API_HEADERS = {
     "accept": "application/json",
-    "Authorization": "Bearer XXXX"
+    "Authorization": "Bearer TOKEN"  # Replace with your token
 }
-# The font used
-truetype_url = 'https://github.com/googlefonts/roboto/raw/main/src/hinted/Roboto-Light.ttf'
+TRUETYPE_URL = 'https://github.com/googlefonts/roboto/raw/main/src/hinted/Roboto-Light.ttf'
+BACKGROUND_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tmdb_backgrounds")
 
-# Endpoint for trending shows
-trending_movies_url = f'{url}trending/movie/week?language=en-US'
-trending_tvshows_url = f'{url}trending/tv/week?language=en-US'
+# Edit to False if you don't want to automatically share the images with your Android TV device
+SHARE_TO_TV = True
 
-# Fetching trending movies and TV shows
-def fetch_trending(url):
-    """Fetches trending movies or TV shows"""
-    response = requests.get(url, headers=headers)
-    return response.json().get('results', [])
+# Edit to set the number of pages, which dictates the amount of wallpapers generated
+# Each page has 20 items
+# Setting either option to 0 won't generate wallpapers for said option, in case you only want movies or only tv shows
+num_pages_movies = 1
+num_pages_tvshows = 1
 
-trending_movies = fetch_trending(trending_movies_url)
-trending_tvshows = fetch_trending(trending_tvshows_url)
+# Ensure the output folder exists
+os.makedirs(BACKGROUND_DIR, exist_ok=True)
 
-# Fetching genres for movies and TV shows
-def fetch_genres(media_type):
-    """Fetches genres for movies or TV shows"""
-    genres_url = f'{url}genre/{media_type}/list?language=en-US'
-    response = requests.get(genres_url, headers=headers)
+def fetch_trending(url: str, num_pages: int = 1) -> List[Dict]:
+    """Fetches trending movies or TV shows."""
+    all_results = []
+    for page in range(1, num_pages + 1):
+        response = requests.get(f"{url}&page={page}", headers=API_HEADERS)
+        response.raise_for_status()  # Raise error for bad responses
+        all_results.extend(response.json().get('results', []))
+    return all_results
+
+def fetch_genres(media_type: str) -> Dict[int, str]:
+    """Fetches genres for movies or TV shows."""
+    genres_url = f'{API_URL}genre/{media_type}/list?language=en-US'
+    response = requests.get(genres_url, headers=API_HEADERS)
+    response.raise_for_status()
     return {genre['id']: genre['name'] for genre in response.json().get('genres', [])}
 
-movie_genres = fetch_genres("movie")
-tv_genres = fetch_genres("tv")
-
-# Fetching TV show and movie details
-def fetch_details(media_type, media_id):
-    """Fetches details of a movie or TV show"""
-    details_url = f'{url}{media_type}/{media_id}?language=en-US'
-    response = requests.get(details_url, headers=headers)
+def fetch_details(media_type: str, media_id: int) -> Dict:
+    """Fetches details of a movie or TV show."""
+    details_url = f'{API_URL}{media_type}/{media_id}?language=en-US'
+    response = requests.get(details_url, headers=API_HEADERS)
+    response.raise_for_status()
     return response.json()
 
-# Create a directory to save the backgrounds
-background_dir = "tmdb_backgrounds"
-# Clear the contents of the folder
-if os.path.exists(background_dir):
-    shutil.rmtree(background_dir)
-os.makedirs(background_dir, exist_ok=True)
-
-# Resize image
-def resize_image(image, height):
-    """Resizes an image"""
+def resize_image(image: Image.Image, height: int) -> Image.Image:
+    """Resizes an image to a specified height while maintaining aspect ratio."""
     ratio = height / image.height
     width = int(image.width * ratio)
     return image.resize((width, height))
 
-# Clean filename
-def clean_filename(filename):
-    """Cleans a filename"""
+def clean_filename(filename: str) -> str:
+    """Cleans a filename to ensure it's safe for the filesystem."""
     return "".join(c if c.isalnum() or c in "._-" else "_" for c in filename)
 
-# Process image
-def process_image(image_url, title, is_movie, genre, year, rating, duration=None, seasons=None):
+def draw_text(draw: ImageDraw.Draw, position: Tuple[int, int], text: str, font: ImageFont.ImageFont, shadow_color: str, main_color: str, shadow_offset: int = 2):
+    """Draws text with shadow effect."""
+    draw.text((position[0] + shadow_offset, position[1] + shadow_offset), text, font=font, fill=shadow_color)
+    draw.text(position, text, font=font, fill=main_color)
+        
+def process_image(image_url, title, overview, is_movie, genre, year, rating, duration=None, seasons=None):
     """
     Processes and saves an image.
 
@@ -99,9 +99,9 @@ def process_image(image_url, title, is_movie, genre, year, rating, duration=None
         draw = ImageDraw.Draw(bckg)
         
         # Define fonts and colors
-        font_title = ImageFont.truetype(urlopen(truetype_url), size=190)
-        font_overview = ImageFont.truetype(urlopen(truetype_url), size=45)
-        font_custom = ImageFont.truetype(urlopen(truetype_url), size=60)
+        font_title = ImageFont.truetype(urlopen(TRUETYPE_URL), size=190)
+        font_overview = ImageFont.truetype(urlopen(TRUETYPE_URL), size=45)
+        font_custom = ImageFont.truetype(urlopen(TRUETYPE_URL), size=60)
         shadow_color = "black"
         main_color = "white"
         overview_color = (150,150,150)
@@ -129,7 +129,7 @@ def process_image(image_url, title, is_movie, genre, year, rating, duration=None
         genre_text = genre
         additional_info = duration if is_movie else f"{seasons} {'Season' if seasons == 1 else 'Seasons'}"
         rating_text = f"TMDB: {rating}"
-        year_text = year[:7]
+        year_text = f"{year[5:7]}-{year[:4]}"
         info_text = f"{genre_text}  •  {year_text}  •  {additional_info}  •  {rating_text}"
         
         # Draw metadata
@@ -141,7 +141,7 @@ def process_image(image_url, title, is_movie, genre, year, rating, duration=None
         draw.text(custom_position, "Now Trending on", font=font_custom, fill=metadata_color)
         
         # Save the resized image
-        filename = os.path.join(background_dir, f"{clean_filename(title)}.jpg")
+        filename = os.path.join(BACKGROUND_DIR, f"{clean_filename(title)}.jpg")
         bckg = bckg.convert('RGB')
         bckg.save(filename)
         print(f"Image saved: {filename}")
@@ -149,58 +149,63 @@ def process_image(image_url, title, is_movie, genre, year, rating, duration=None
         print(f"Failed to download background for {title}")
 
 
-# Process each trending movie
-for movie in trending_movies:
-    # Extract movie details
-    title = movie['title']
-    overview = movie['overview']
-    year = movie['release_date']
-    rating = round(movie['vote_average'], 1)
-    genre = ', '.join([movie_genres[genre_id] for genre_id in movie['genre_ids']])
-    
-    # Fetch additional movie details
-    movie_details = fetch_details("movie", movie['id'])
-    duration = movie_details.get('runtime', "N/A")
-    
-    # Format duration as hours and minutes
-    if duration != "N/A":
-        hours = duration // 60
-        minutes = duration % 60
-        duration = f"{hours}h{minutes}min"
-        
-    # Check if backdrop image is available
-    backdrop_path = movie['backdrop_path']
-    if backdrop_path:
-        # Construct image URL
-        image_url = f"https://image.tmdb.org/t/p/original{backdrop_path}"
-        
-        # Process the image
-        process_image(image_url, title, is_movie=True, genre=genre, year=year, rating=rating, duration=duration)
-    else:
-        # Print error message if no backdrop image found
-        print(f"No backdrop image found for {title}")
+def process_media(current_media: List[Dict], is_movie: bool, genres: Dict[int, str]):
+    """Processes and saves media images (movies or TV shows)."""
+    for media in current_media:
+        title = media['title'] if is_movie else media['name']
+        overview = media['overview']
+        cleaned_title = clean_filename(title)
+        backdrop_path = media.get('backdrop_path')
 
-# Process trending TV shows
-for tvshow in trending_tvshows:
-    # Extract TV show details
-    title = textwrap.shorten(tvshow['name'], width=38)
-    overview = tvshow['overview']
-    year = tvshow['first_air_date']
-    rating = round(tvshow['vote_average'], 1)
-    genre = ', '.join([tv_genres[genre_id] for genre_id in tvshow['genre_ids']])
-    
-    # Fetch additional TV show details
-    tv_details = fetch_details("tv", tvshow['id'])
-    seasons = tv_details.get('number_of_seasons', 0)
-    
-    # Check if backdrop image is available
-    backdrop_path = tvshow['backdrop_path']
-    if backdrop_path:
-        # Construct image URL
-        image_url = f"https://image.tmdb.org/t/p/original{backdrop_path}"
-        
-        # Process the image
-        process_image(image_url, title, is_movie=False, genre=genre, year=year, rating=rating, seasons=seasons)
-    else:
-        # Print error message if no backdrop image found
-        print(f"No backdrop image found for {title}")
+        if backdrop_path:
+            image_url = f"https://image.tmdb.org/t/p/original{backdrop_path}"
+            if f"{cleaned_title}.jpg" not in existing_files:
+                if is_movie:
+                    movie_details = fetch_details("movie", media['id'])
+                    duration = movie_details.get('runtime', "N/A")
+                    if duration != "N/A":
+                        hours = duration // 60
+                        minutes = duration % 60
+                        duration = f"{hours}h{minutes}min"
+                    genre_text = ', '.join(genres[genre_id] for genre_id in media['genre_ids'])
+                    process_image(image_url, title, overview, is_movie, genre_text, 
+                                  media.get('release_date', ""), 
+                                  round(media['vote_average'], 1), 
+                                  duration)
+                else:
+                    tv_details = fetch_details("tv", media['id'])
+                    seasons = tv_details.get('number_of_seasons', 0)
+                    genre_text = ', '.join(genres[genre_id] for genre_id in media['genre_ids'])
+                    process_image(image_url, title, overview, is_movie, genre_text, 
+                                  media.get('first_air_date', ""), 
+                                  round(media['vote_average'], 1), 
+                                  seasons=seasons)
+                
+
+def get_current_trending() -> Tuple[List[Dict], List[Dict]]:
+    """Gets the current trending movies and TV shows."""
+    current_movies = fetch_trending(f"{API_URL}trending/movie/week?language=en-US", num_pages_movies)
+    current_tvshows = fetch_trending(f"{API_URL}trending/tv/week?language=en-US", num_pages_tvshows)
+    return current_movies, current_tvshows
+
+# Get current trending media
+current_movies, current_tvshows = get_current_trending()
+existing_files = set(os.listdir(BACKGROUND_DIR))
+
+# Process movies
+process_media(current_movies, is_movie=True, genres=fetch_genres("movie"))
+
+# Process TV shows
+process_media(current_tvshows, is_movie=False, genres=fetch_genres("tv"))
+
+# Remove files that are no longer in the current trending lists
+current_titles = {clean_filename(media['title']) if 'title' in media else clean_filename(media['name']) for media in current_movies + current_tvshows}
+
+for file_name in existing_files:
+    if file_name[:-4] not in current_titles:  # Exclude the .jpg extension for comparison
+        os.remove(os.path.join(BACKGROUND_DIR, file_name))
+        print(f"Removed unused file: {file_name}")
+
+# Share to Android TV folder
+if(SHARE_TO_TV):
+    ShieldShare.sync_folders(BACKGROUND_DIR)
